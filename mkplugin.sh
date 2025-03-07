d2ae40d2f1e771b563db1313b5addc88a0f13c7f
Spectrasonic <spectrasonic117@gmail.com>
2025-03-06 21:50:12 -0600

    add send message and send actionbar to player and broadcast

--- mkplugin.sh
+++ mkplugin.sh
@@ -215,10 +215,13 @@ printf "package com.spectrasonic.${PROJECT_NAME}.Utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.Title;
+import net.kyori.adventure.title.Title.Times;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.time.Duration;
 
 public final class MessageUtils {
 
@@ -297,6 +300,48 @@ public final class MessageUtils {
             Bukkit.getConsoleSender().sendMessage(miniMessage.deserialize(message));
         }
     }
+
+    public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+    final Component titleComponent = miniMessage.deserialize(PREFIX + title);
+    final Component subtitleComponent = miniMessage.deserialize(PREFIX + subtitle);
+    player.showTitle(Title.title(titleComponent, subtitleComponent, Times.times(
+        Duration.ofMillis(fadeIn * 50L),
+        Duration.ofMillis(stay * 50L),
+        Duration.ofMillis(fadeOut * 50L)
+    )));
+}
+
+    public static void sendActionBar(Player player, String message) {
+        player.sendActionBar(miniMessage.deserialize(PREFIX + message));
+    }
+
+    public static void broadcastTitle(String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+        final Component titleComponent = miniMessage.deserialize(PREFIX + title);
+        final Component subtitleComponent = miniMessage.deserialize(PREFIX + subtitle);
+        final Title formattedTitle = Title.title(titleComponent, subtitleComponent, Times.times(
+            Duration.ofMillis(fadeIn * 50L),
+            Duration.ofMillis(stay * 50L),
+            Duration.ofMillis(fadeOut * 50L)
+        ));
+
+        // Uso - Send Title to players
+        // MiniMessageUtils.sendTitle(player, 
+        //     \"<gold>¡Alerta!</gold>\", 
+        //     \"<red>Mensaje importante</red>\", 
+        //     2, 40, 2
+        // );
+        
+        Bukkit.getOnlinePlayers().forEach(player -> player.showTitle(formattedTitle));
+    }
+
+    public static void broadcastActionBar(String message) {
+        final Component component = miniMessage.deserialize(PREFIX + message);
+        Bukkit.getOnlinePlayers().forEach(player -> player.sendActionBar(component));
+    }
+
+    // Uso Broadcast ActionBAR
+    // MiniMessageUtils.broadcastActionBar(\"<yellow>¡Evento especial activado!</yellow>\");
+
 }
 " > ${PWD}/src/main/java/com/spectrasonic/${PROJECT_NAME}/Utils/MessageUtils.java
 
